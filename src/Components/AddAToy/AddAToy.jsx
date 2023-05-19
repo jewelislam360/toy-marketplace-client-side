8fc28f917924eb5affc04cb7659ab60038efa5bc
jewelislam360 <jewelislam.it360@gmail.com>
2023-05-19 21:51:26 +0600

    add a toy form setup

--- src/Components/AddAToy/AddAToy.jsx
+++ src/Components/AddAToy/AddAToy.jsx
@@ -3,7 +3,79 @@ import React from 'react';
 const AddAToy = () => {
     return (
         <div>
-            <h2>this is  add a toy page </h2>
+            <form >
+                {/* name and photo url */}
+
+                <div className='flex gap-4'>
+                    <div className="form-control w-[50%]">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input type="text" placeholder="Photo Url" className="input input-bordered input-secondary w-[100%]" />
+                    </div>
+                    <div className="form-control w-[50%]">
+                        <label className="label">
+                            <span className="label-text">Photo Url</span>
+                        </label>
+                        <input type="text" placeholder="Photo Url" className="input input-bordered input-secondary w-[100%]" />
+                    </div>
+                </div>
+                {/* seller name and email */}
+
+                <div className='flex gap-4'>
+                    <div className="form-control w-[50%]">
+                        <label className="label">
+                            <span className="label-text">Seller Name</span>
+                        </label>
+                        <input type="text" placeholder="Seller Name" className="input input-bordered input-secondary w-[100%]" />
+                    </div>
+                    <div className="form-control w-[50%]">
+                        <label className="label">
+                            <span className="label-text">Seller email</span>
+                        </label>
+                        <input type="email" placeholder="Seller email" className="input input-bordered input-secondary w-[100%]" />
+                    </div>
+                </div>
+                <div className="form-control w-[50%]">
+
+                </div>
+                {/* Category  and Price */}
+
+
+                <div className='flex gap-4'>
+                    <div className="form-control w-[50%]">
+                        <label className="label">
+                            <span className="label-text">Category</span>
+                        </label>
+                        <div className="input-group w-[100%]">
+                            <select name='Category' className="select-secondary select select-bordered w-[100%]">
+                                <option disabled selected>Pick category</option>
+                                <option>sports car</option>
+                                <option>truck</option>
+                                <option>regular car</option>
+                                <option>mini fire truck</option>
+                                <option>mini police car</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="form-control w-[50%]">
+                        <label className="label">
+                            <span className="label-text">Price</span>
+                        </label>
+                        <input type="text" placeholder="Price" className="input input-bordered input-secondary w-[100%]" />
+                    </div>
+                </div>
+                {/* Toy Description*/}
+                <div className="form-control w-[50%] mb-6">
+                    <label className="label">
+                        <span className="label-text">Toy Description</span>
+                    </label>
+                    <input type="area" placeholder="description" className="input input-bordered input-secondary w-[100%]" />
+                </div>
+
+                <button className="btn btn-secondary w-[100%]">Button</button>
+
+            </form>
         </div>
     );
 };
