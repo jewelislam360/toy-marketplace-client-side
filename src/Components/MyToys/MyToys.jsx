34d723350e75295428015b6d07b9deec33e30fae
jewelislam360 <jewelislam.it360@gmail.com>
2023-05-20 19:15:29 +0600

    by meail get data

--- src/Components/MyToys/MyToys.jsx
+++ src/Components/MyToys/MyToys.jsx
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../Providers/AuthProvider';
 
 const MyToys = () => {
+    const { user } = useContext(AuthContext);
+    const [myToys, setMyToys]= useState();
+
+
+
+    useEffect(()=>{
+        fetch(`http://localhost:5000/myToys/${user?.email}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            setMyToys(data);
+
+        })
+    },[user])
 
     return (
         <div>
